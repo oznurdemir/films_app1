810266935bce937ba719ad91210099b7ac65a69b
oznur.demir <oznur.demir@beymen.com>
2024-09-20 14:06:02 +0300

    Updated homepage.dart and main.dart

--- lib/ui/views/homepage.dart
+++ lib/ui/views/homepage.dart
@@ -12,6 +12,7 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("HomePage"),),
+      body: Center(),
     );
   }
 }
